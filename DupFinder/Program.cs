a0ac4e2a190daebffcaaded2b5c64395068d1cc8
Андрей Сергеевич Кайбазаков <ask@sobes.tric.ru>
2020-02-17 14:25:52 +0700

    Добавьте файлы проекта.

---
+++ DupFinder/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DupFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            args = new string[5] { "123", "123", "12", "43", "43" };
+            if (args.Length > 0)
+            {
+                if (args[0] == "--help" || args[0] == "-help")
+                {
+                    Console.WriteLine("Help:");
+                    return;
+                }
+            }
+            else
+                return;
+            List<String> list = new List<string>();
+            list.Add(args[0]);
+            Console.Write(args[0] + " ");
+            int j2;
+            for (int i = 1; i < args.Length; i++)
+            {
+                j2 = list.Count;
+                for (int j = 0; j < j2; j++)
+                {
+                    if (args[i] != list[j])
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        list.Add(args[i]);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    }
+                }
+                Console.Write(args[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
